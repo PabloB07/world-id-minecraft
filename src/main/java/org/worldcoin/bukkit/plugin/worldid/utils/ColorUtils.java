c52816d31bfe503224f41fb0130c8703d22f5d7e
PabloB07 <pabloblanco0798@gmail.com>
2024-10-16 15:58:10 -0300

    Added ColorUtils class for hex colors

--- src/main/java/org/worldcoin/bukkit/plugin/worldid/utils/ColorUtils.java
+++ src/main/java/org/worldcoin/bukkit/plugin/worldid/utils/ColorUtils.java
@@ -2,172 +2,159 @@ package org.worldcoin.bukkit.plugin.worldid.utils;
 
 import net.md_5.bungee.api.ChatColor;
 
-import java.awt.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ColorUtils {
+    private static final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
 
-    /*
-     *
-     * Class Values
-     *
-     * */
+    public static String colorize(final String textToTranslate) {
+        final char altColorChar = '&'; // & -> §, &chello -> RED, &#cc44ffHello -> colorize Hello
+        final StringBuilder b = new StringBuilder();
+        final char[] mess = textToTranslate.toCharArray();
+        boolean color = false, hashtag = false, doubleTag = false;
+        char tmp; // Used in loops
 
-    private static Method COLOR_FROM_CHAT_COLOR;
-    private static Method CHAT_COLOR_FROM_COLOR;
-    private static final boolean hexSupport;
-    private static final Pattern gradient = Pattern.compile("<(#[A-Za-z0-9]{6})>(.*?)</(#[A-Za-z0-9]{6})>");
-    private static final Pattern legacyGradient = Pattern.compile("<(&[A-Za-z0-9])>(.*?)</(&[A-Za-z0-9])>");
-    private static final Pattern rgb = Pattern.compile("&\\{(#......)}");
+        for (int i = 0; i < mess.length; ) { // i increment is handled case by case for speed
 
-    static {
-        try {
-            COLOR_FROM_CHAT_COLOR = ChatColor.class.getDeclaredMethod("getColor");
-            CHAT_COLOR_FROM_COLOR = ChatColor.class.getDeclaredMethod("of", Color.class);
-        } catch (NoSuchMethodException e) {
-            COLOR_FROM_CHAT_COLOR = null;
-            CHAT_COLOR_FROM_COLOR = null;
-        }
-        hexSupport = CHAT_COLOR_FROM_COLOR != null;
-    }
+            final char c = mess[i];
 
-    /*
-     *
-     * Class API
-     *
-     * */
+            if (doubleTag) { // DoubleTag module
+                doubleTag = false;
 
-    public static String colorize(String text) {
-        return colorize(text, '&');
-    }
+                final int max = i + 3;
 
-    public static String colorize(String text, char colorSymbol) {
-        Matcher g = gradient.matcher(text);
-        Matcher l = legacyGradient.matcher(text);
-        Matcher r = rgb.matcher(text);
-        while (g.find()) {
-            Color start = Color.decode(g.group(1));
-            String between = g.group(2);
-            Color end = Color.decode(g.group(3));
-            if (hexSupport) text = text.replace(g.group(0), rgbGradient(between, start, end, colorSymbol));
-            else text = text.replace(g.group(0), between);
-        }
-        while (l.find()) {
-            char first = l.group(1).charAt(1);
-            String between = l.group(2);
-            char second = l.group(3).charAt(1);
-            ChatColor firstColor = ChatColor.getByChar(first);
-            ChatColor secondColor = ChatColor.getByChar(second);
-            if (firstColor == null) firstColor = ChatColor.WHITE;
-            if (secondColor == null) secondColor = ChatColor.WHITE;
-            if (hexSupport) text = text.replace(l.group(0), rgbGradient(between, fromChatColor(firstColor), fromChatColor(secondColor), colorSymbol));
-            else text = text.replace(l.group(0), between);
-        }
-        while (r.find()) {
-            if (hexSupport) {
-                ChatColor color = fromColor(Color.decode(r.group(1)));
-                text = text.replace(r.group(0), color + "");
-            } else {
-                text = text.replace(r.group(0), "");
-            }
-        }
-        return ChatColor.translateAlternateColorCodes(colorSymbol, text);
-    }
+                if (max <= mess.length) {
+                    // There might be a hex color here
+                    boolean match = true;
 
-    public static String removeColors(String text) {
-        return ChatColor.stripColor(text);
-    }
-
-    public static List<Character>charactersWithoutColors(String text) {
-        text = removeColors(text);
-        final List<Character> result = new ArrayList<>();
-        for (char var : text.toCharArray()) {
-            result.add(var);
-        }
-        return result;
-    }
-
-    public static List<String> charactersWithColors(String text) {
-        return charactersWithColors(text, '§');
-    }
-
-    public static List<String> charactersWithColors(String text, char colorSymbol) {
-        final List<String> result = new ArrayList<>();
-        StringBuilder colors = new StringBuilder();
-        boolean colorInput = false;
-        boolean reading = false;
-        for (char var : text.toCharArray()) {
-            if (colorInput) {
-                colors.append(var);
-                colorInput = false;
-            } else {
-                if (var == colorSymbol) {
-                    if (!reading) {
-                        colors = new StringBuilder();
+                    for (int n = i; n < max; n++) {
+                        tmp = mess[n];
+                        // The order of the checks below is meant to improve performances (i.e. capital letters check is at the end)
+                        if (!((tmp >= '0' && tmp <= '9') || (tmp >= 'a' && tmp <= 'f') || (tmp >= 'A' && tmp <= 'F'))) {
+                            // It wasn't a hex color, appending found chars to the StringBuilder and continue the for loop
+                            match = false;
+                            break;
+                        }
                     }
-                    colorInput = true;
-                    reading = true;
-                    colors.append(var);
-                } else {
-                    reading = false;
-                    result.add(colors.toString() + var);
+
+                    if (match) {
+                        b.append(ChatColor.COLOR_CHAR);
+                        b.append('x');
+
+                        // Copy colors with a color code in between
+                        for (; i < max; i++) {
+                            tmp = mess[i];
+                            b.append(ChatColor.COLOR_CHAR);
+                            b.append(tmp);
+                            // Double the color code
+                            b.append(ChatColor.COLOR_CHAR);
+                            b.append(tmp);
+                        }
+
+                        // i increment has been already done
+                        continue;
+                    }
                 }
+
+                b.append(altColorChar);
+                b.append("##");
+                // Malformed hex, let's carry on checking mess[i]
             }
+
+            if (hashtag) { // Hashtagmodule
+                hashtag = false;
+
+                // Check for double hashtag (&##123 => &#112233)
+                if (c == '#') {
+                    doubleTag = true;
+                    i++;
+                    continue;
+                }
+
+                final int max = i + 6;
+
+                if (max <= mess.length) {
+                    // There might be a hex color here
+                    boolean match = true;
+
+                    for (int n = i; n < max; n++) {
+                        tmp = mess[n];
+                        // The order of the checks below is meant to improve performances (i.e. capital letters check is at the end)
+                        if (!((tmp >= '0' && tmp <= '9') || (tmp >= 'a' && tmp <= 'f') || (tmp >= 'A' && tmp <= 'F'))) {
+                            // It wasn't a hex color, appending found chars to the StringBuilder and continue the for loop
+                            match = false;
+                            break;
+                        }
+                    }
+
+                    if (match) {
+                        b.append(ChatColor.COLOR_CHAR);
+                        b.append('x');
+
+                        // Copy colors with a color code in between
+                        for (; i < max; i++) {
+                            b.append(ChatColor.COLOR_CHAR);
+                            b.append(mess[i]);
+                        }
+                        // i increment has been already done
+                        continue;
+                    }
+                }
+
+                b.append(altColorChar);
+                b.append('#');
+                // Malformed hex, let's carry on checking mess[i]
+            }
+
+            if (color) { // Color module
+                color = false;
+
+                if (c == '#') {
+                    hashtag = true;
+                    i++;
+                    continue;
+                }
+
+                // The order of the checks below is meant to improve performances (i.e. capital letters check is at the end)
+                if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || c == 'r' || (c >= 'k' && c <= 'o') || (c >= 'A' && c <= 'F') || c == 'R' || (c >= 'K' && c <= 'O')) {
+                    b.append(ChatColor.COLOR_CHAR);
+                    b.append(c);
+                    i++;
+                    continue;
+                }
+
+                b.append(altColorChar);
+                // Not a valid color, let's carry on checking mess[i]
+            }
+
+            // Base case
+            if (c == altColorChar) { // c == '&'
+                color = true;
+                i++;
+                continue;
+            }
+
+            // None matched, append current character
+            b.append(c);
+            i++;
+
         }
-        return result;
+
+        // Append '&' if '&' was the last character of the string
+        if (color)
+            b.append(altColorChar);
+        else // color and hashtag cannot be true at the same time
+            // Append "&#" if "&#" were the last characters of the string
+            if (hashtag) {
+                b.append(altColorChar);
+                b.append('#');
+            } else // color, hashtag, and doubleTag cannot be true at the same time
+                // Append "&##" if "&##" were the last characters of the string
+                if (doubleTag) {
+                    b.append(altColorChar);
+                    b.append("##");
+                }
+
+        return b.toString();
     }
-
-    /*
-     *
-     * Class Utilities
-     *
-     * */
-
-    private static String rgbGradient(String text, Color start, Color end, char colorSymbol) {
-        final StringBuilder builder = new StringBuilder();
-        text = ChatColor.translateAlternateColorCodes(colorSymbol, text);
-        final List<String> characters = charactersWithColors(text);
-        final double[] red = linear(start.getRed(), end.getRed(), characters.size());
-        final double[] green = linear(start.getGreen(), end.getGreen(), characters.size());
-        final double[] blue = linear(start.getBlue(), end.getBlue(), characters.size());
-        if (text.length() == 1) {
-            return fromColor(end) + text;
-        }
-        for (int i = 0; i < characters.size(); i++) {
-            String currentText = characters.get(i);
-            ChatColor current = fromColor(new Color((int) Math.round(red[i]), (int) Math.round(green[i]), (int) Math.round(blue[i])));
-            builder.append(current).append(currentText.replace("§r", ""));
-        }
-        return builder.toString();
-    }
-
-    private static double[] linear(double from, double to, int max) {
-        final double[] res = new double[max];
-        for (int i = 0; i < max; i++) {
-            res[i] = from + i * ((to - from) / (max - 1));
-        }
-        return res;
-    }
-
-    private static Color fromChatColor(ChatColor color) {
-        try {
-            return (Color) COLOR_FROM_CHAT_COLOR.invoke(color);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static ChatColor fromColor(Color color) {
-        try {
-            return (ChatColor) CHAT_COLOR_FROM_COLOR.invoke(null, color);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
